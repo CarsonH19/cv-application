dc2b18c6fdafc321b786a4a5b1be1d87455bc823
CarsonH19 <questline.carson@gmail.com>
2024-03-09 23:52:16 +0900

    feat: main codes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// CSS Imports
+import './styles/forms.css';
+import './styles/resume.css';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
